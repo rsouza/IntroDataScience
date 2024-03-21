abfd2da1c517688b1921ddfc67d4b5e027decce7
Renato Rocha Souza <renato.rocha-souza@rbinternational.com>
2024-03-21 14:37:25 +0100

    Fixing bugs AutoML

--- 2_virtual_classroom_materials/day_5_supervised_unsupervised_learning/1_supervised_learning/Databricks/4a_AutoML_TPOT.py
+++ 2_virtual_classroom_materials/day_5_supervised_unsupervised_learning/1_supervised_learning/Databricks/4a_AutoML_TPOT.py
@@ -5,15 +5,6 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC In this notebook, we will explore how to use **TPOT** to automatically optimize machine learning pipelines.
-# MAGIC
-# MAGIC TPOT, which stands for **Tree-based Pipeline Optimization Tool**, is an open-source AutoML library in Python. It utilizes **genetic programming** to automate the process of feature engineering, model selection, and hyperparameter tuning. TPOT generates and evaluates a population of pipelines, evolving them over generations to identify the most effective combination of data preprocessing steps and machine learning models.
-# MAGIC
-# MAGIC Before we preceed, let's install TPOT library.
-
-# COMMAND ----------
-
-# MAGIC %md
 # MAGIC In this notebook, we will explore how to use [**TPOT**](https://epistasislab.github.io/tpot/) to automatically optimize machine learning pipelines.
 # MAGIC
 # MAGIC TPOT, which stands for **Tree-based Pipeline Optimization Tool**, is an open-source AutoML library in Python. It utilizes **genetic programming** to automate the process of feature engineering, model selection, and hyperparameter tuning. TPOT generates and evaluates a population of pipelines, evolving them over generations to identify the most effective combination of data preprocessing steps and machine learning models.
@@ -23,11 +14,6 @@
 # COMMAND ----------
 
 !pip install -q tpot
-
-# COMMAND ----------
-
-# You only need to run this cell after installing the optuna package on Databricks
-dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -49,8 +35,33 @@ from sklearn.model_selection import train_test_split
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC We will use the Boston dataset for the regression.
+
+# COMMAND ----------
+
 # Load Boston dataset
 boston_df = pd.read_csv("../../../../Data/Boston.csv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC It is important to preprocess your data before using TPOT. We need to take care of missing values and categorical variables. Let's look at the summary of our DataFrame.
+
+# COMMAND ----------
+
+boston_df.head()
+
+# COMMAND ----------
+
+boston_df.info()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We can see here that all entries have non-null values and float data type for all columns. It means, the only thing we need to do is splitting the data into training and testing sets.
+
+# COMMAND ----------
 
 X = boston_df.iloc[:, 1:14]
 y = boston_df.iloc[:, -1]
@@ -73,22 +84,55 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # MAGIC * cv: The number of cross-validation folds to use during pipeline evaluation.
 # MAGIC * verbosity: The level of verbosity for output during optimization (higher values provide more details).
 # MAGIC
+# MAGIC TPOT effectiveness improves with more generations, but the trade-off is longer processing time.
+# MAGIC
+# MAGIC **How can we control the execution time?**
+# MAGIC
+# MAGIC We can adjust certain parameters to control TPOT execution time:
+# MAGIC
+# MAGIC * max_time_mins: overrides the generations parameter, specifying the time TPOT runs.
+# MAGIC * max_eval_time_mins: how many minutes TPOT spends evaluating a single pipeline.
+# MAGIC * early_stop: determines when TPOT ends optimization if no improvement occurs.
+# MAGIC * n_jobs: specifies the number of procedures used in parallel during optimization.
+# MAGIC * subsample: fraction of training samples used during optimization.
+# MAGIC
+# MAGIC
+# MAGIC But keep in mind that constraining execution time limits TPOT's ability to explore all potential pipelines thoroughly. Consequently, the model suggested within this timeframe may be not the best fit for the dataset. However, if time is sufficient, TPOT can offer something very close to the best model.
+# MAGIC
 # MAGIC Now, let's create an instance of TPOTRegressor and let it search for the best regression pipeline on the Boston dataset:
 
 # COMMAND ----------
 
 # Create a TPOTRegressor instance
-tpot_reg = TPOTRegressor(generations=5, population_size=50, verbosity=2, random_state=42)
+tpot_reg = TPOTRegressor(generations=5, population_size=20, verbosity=2, random_state=42, max_time_mins=5, scoring="neg_mean_squared_error")
 
 # Fit TPOT on the training data for regression
-tpot_reg.fit(X_train, y_train)
+tpot_reg.fit(X_train.values, y_train)
+print("Negative MSE on the test set: ", tpot_reg.score(X_test.values, y_test))
 
 # COMMAND ----------
 
-# Evaluate the best regression pipeline on the test set
-best_pipeline_reg = tpot_reg.fitted_pipeline_
-test_score_reg = best_pipeline_reg.score(X_test, y_test)
-print(f'Test Set R^2 Score (Regression): {test_score_reg:.3f}')
+# MAGIC %md
+# MAGIC *Note that the default scoring function for regression is negative mean squared error. It is recommended to use the negative version of mean squared error and related metrics so TPOT will minimize (instead of maximize) the metric. You can also create and use your [custom metric](https://epistasislab.github.io/tpot/using/#scoring-functions).*
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC There are 4 main functions we can use:
+# MAGIC
+# MAGIC * fit - Run the TPOT optimization process on the given training data.
+# MAGIC * predict - Use the optimized pipeline to predict the target values for a feature set.
+# MAGIC * score - Returns the optimized pipeline's score on the given testing data using the user-specified scoring function.
+# MAGIC * export - Export the optimized pipeline as Python code.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Let's take a look at the resulting pipeline:
+
+# COMMAND ----------
+
+tpot_reg.fitted_pipeline_
 
 # COMMAND ----------
 
@@ -98,68 +142,52 @@ print(f'Test Set R^2 Score (Regression): {test_score_reg:.3f}')
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC *You may use the documentation as a guide:*
-# MAGIC
-# MAGIC *http://epistasislab.github.io/tpot/api/*
+# MAGIC Scikit-learn includes some [popular datasets](https://scikit-learn.org/stable/datasets/toy_dataset.html). We will load [one of them](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_digits.html) to illustrate how [**TPOTClassifier**](https://epistasislab.github.io/tpot/api/#classification) works.
 
 # COMMAND ----------
 
-# Task: Import titanic.csv dataset
-
-titanic_df = ...
-
-# COMMAND ----------
-
-X = titanic_df[['Sex', 'Embarked', 'Pclass', 'Age', 'Survived']]
-y = titanic_df[['Survived']]
+from sklearn.datasets import load_digits
+digits = load_digits(as_frame=True) 
+#if as_frame parameter is set to True, the data is a pandas DataFrame
+digits.frame
 
 # COMMAND ----------
 
-# Task: split the dataset into train and test sets
-
-...
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=0.8, test_size=0.2)
 
 # COMMAND ----------
 
-# We need to deal with the missing data and transform categorical attributes
+tpot_class = TPOTClassifier(generations=5, population_size=20, cv=5, random_state=42, verbosity=2, max_time_mins=5)
 
-categorical_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
-        ("encoder", OneHotEncoder(handle_unknown="ignore")),
-    ]
-)
+tpot_class.fit(X_train.values, y_train)
 
-numerical_transformer = Pipeline(steps=[("imputer", SimpleImputer())])
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("cat", categorical_transformer, ["Sex", "Embarked"]),
-        ("num", numerical_transformer, ["Age"])
-    ],
-    remainder="passthrough",
-)
-
-X_train = preprocessor.fit_transform(X_train)
-X_test = preprocessor.transform(X_test)
-
-# COMMAND ----------
-
-# Task: Create a TPOTClassifier instance
-...
-
-# Task: Fit TPOT on the training data for classification
-...
-
-# COMMAND ----------
-
-# Task: Evaluate the best classification pipeline on the test set
-...
+print("Accuracy on the test set: ", tpot_class.score(X_test.values, y_test))
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC In this notebook, we used TPOT, a powerful AutoML library, to automate the process of optimizing machine learning pipelines for both regression and classification tasks. We applied TPOT on two different datasets: the Boston dataset for regression and the Titanic dataset for classification.
+# MAGIC *Note that the default scoring function for TPOTClassifier is accuracy. We can also use other built-in function such as 'balanced_accuracy', 'f1', 'precision', 'recall' etc.*
+# MAGIC
+# MAGIC To export the optimized pipeline as Python code we can use the export function:
+
+# COMMAND ----------
+
+# tpot.export('tpot_class_pipeline.py')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Let's get predictions for the test set:
+
+# COMMAND ----------
+
+predictions = tpot_class.fitted_pipeline_.predict(X_test.values)
+predictions
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In this notebook, we used TPOT, a powerful AutoML library, to automate the process of optimizing machine learning pipelines for both regression and classification tasks. We applied TPOT on two different datasets: the Boston dataset for regression and the Digits dataset for classification.
 # MAGIC
 # MAGIC Using TPOT, we significantly reduced the manual effort of hyperparameter tuning and pipeline construction while achieving competitive performance in both tasks. TPOT is a valuable tool for automating the machine learning workflow and is worth exploring further for various datasets and tasks.
 # MAGIC
